75fae181f8c8e7a2e70ed91e51c86fbdbbfb2b2b
Hitesh <hitesh.kumar.cse20@itbhu.ac.in>
2022-06-03 17:05:36 +0530

    make itdsdsds

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,15 +1,19 @@
-from DriveUtil import save_random_file
-import my_bot
+# from DriveUtil import save_random_file
+# import my_bot
 import time
 period = 3600
+#
+# while(True):
+#     print("running")
+#     save_random_file()
+#     try:
+#         my_bot.upload_media()
+#         print("uploaded")
+#     except:
+#         print("didn't uploaded")
+#     my_bot.auto_follow_back()
+#     time.sleep(period)
 
 while(True):
-    print("running")
-    save_random_file()
-    try:
-        my_bot.upload_media()
-        print("uploaded")
-    except:
-        print("didn't uploaded")
-    my_bot.auto_follow_back()
-    time.sleep(period)
+    print("working..........................")
+    time.sleep(period/100)
