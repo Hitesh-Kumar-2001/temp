15117f4c63e1da38f90dc5e20a85d807a3bb7e96
Hitesh <hitesh.kumar.cse20@itbhu.ac.in>
2022-06-08 15:39:44 +0530

    kdfsanmfk

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,19 +1,15 @@
-# from DriveUtil import save_random_file
-# import my_bot
+from DriveUtil import save_random_file
+import my_bot
 import time
 period = 3600
-#
-# while(True):
-#     print("running")
-#     save_random_file()
-#     try:
-#         my_bot.upload_media()
-#         print("uploaded")
-#     except:
-#         print("didn't uploaded")
-#     my_bot.auto_follow_back()
-#     time.sleep(period)
 
-while(True):
-    print("working..........................")
-    time.sleep(period/100)
+
+save_random_file()
+try:
+    my_bot.upload_media()
+    print("uploaded")
+except:
+    print("didn't uploaded")
+my_bot.auto_follow_back()
+
+
