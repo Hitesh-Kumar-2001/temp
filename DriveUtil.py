bbf94b447838446380ca6b4dae697125d738b270
Hitesh <hitesh.kumar.cse20@itbhu.ac.in>
2022-06-19 18:46:45 +0530

    make it better

--- DriveUtil.py
+++ DriveUtil.py
@@ -5,22 +5,27 @@ from Google import Create_Service
 import  pandas as pd
 import sys
 import numpy as np
+import data_retriever
 from googleapiclient.http import MediaIoBaseDownload
 
 file_name = 'image.jpeg'
 token = 'token.json'
 api_name = 'drive'
 api_version = 'v3'
-folder_id = '1wUw7kiET7c7spDZSQRoXjhDlUo5sh6bX'
+folders_ids = data_retriever.get_folder_ids()
 scopes = ['https://www.googleapis.com/auth/drive']
-query = f"parents =  '{folder_id}'"
+
 service = Create_Service(token,api_name,api_version,scopes)
 def get_ids():
-        response = service.files().list(q=query,pageSize=1000).execute()
-        files = response.get('files')
-        next_page_token = response.get('nextPageToken')
-        df = pd.DataFrame(files)
-        ids = df['id'].tolist()
+        ids = []
+        for folder in folders_ids:
+                query = f"parents =  '{folder}'"
+                response = service.files().list(q=query,pageSize=1000).execute()
+                files = response.get('files')
+                next_page_token = response.get('nextPageToken')
+                df = pd.DataFrame(files)
+                temp = df['id'].tolist()
+                ids = ids + temp
         return ids
 
 
--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import my_bot
 import time
 period = 3600
 
-while(True):
-    save_random_file()
-    try:
-        my_bot.upload_media()
-        print("uploaded")
-    except:
-        print("didn't uploaded")
-    my_bot.auto_follow_back()
-    time.sleep(period)
+
+save_random_file()
+try:
+    my_bot.upload_media()
+    print("uploaded")
+except:
+    print("didn't uploaded")
+my_bot.auto_follow_back()
+
+
+
+
 
 
