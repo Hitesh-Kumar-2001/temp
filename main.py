4d3fe998f33e278713a29ad9d2728637ff35ca78
Hitesh <hitesh.kumar.cse20@itbhu.ac.in>
2022-06-19 18:48:13 +0530

    make it better

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import my_bot
 import time
 period = 3600
 
-
-save_random_file()
-try:
-    my_bot.upload_media()
-    print("uploaded")
-except:
-    print("didn't uploaded")
-my_bot.auto_follow_back()
+while(True)
+    save_random_file()
+    try:
+        my_bot.upload_media()
+        print("uploaded")
+    except:
+        print("didn't uploaded")
+    my_bot.auto_follow_back()
+    time.sleep(period)
 
 
 
